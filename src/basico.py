580cbf0be4fb4572af04d50358f8e675306f6641
Murilo-Maua <murilo.carvalho@maua.br>
2022-09-22 11:41:21 -0300

    adicionando programa básico 1

---
+++ src/basico.py
@@ -0,0 +1,5 @@
+# Script básico para estudar algumas interações com Python
+print("Ola Mundo!")
+# Para executar o programa, digitar no prompt: python ./src/basico.py
+nome = input("Qual o seu nome?")
+print(f"Ola {nome}!")
